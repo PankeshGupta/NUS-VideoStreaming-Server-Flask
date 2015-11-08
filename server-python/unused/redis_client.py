dcbd90fa6f839b89baf006bd5dc09ba4eea9dd41
lpthanh <lpthanh@gmail.com>
2015-11-09 03:25:45 +0800

    Removed redis.

--- server-python/unused/redis_client.py
+++ server-python/unused/redis_client.py
@@ -7,6 +7,10 @@ from settings import REDIS_PORT
 # But I found out that the server has both memcached and gearman installed,
 # so this is no longer needed, as it will require installing another server.
 
+# Also, since each process importing this file will try to launch its own
+# instance of Redis on the same local port, this will be a problem once worker
+# processes are started.
+
 patch.patch_redis()
 
 redis = Redis(serverconfig={'port': REDIS_PORT})
