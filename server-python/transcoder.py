c09558684ff6dfcc3aee0a19cb38e7826f7e3423
lpthanh <lpthanh@gmail.com>
2015-11-09 02:55:07 +0800

    Added the correct command to transcode a file.

--- server-python/transcoder.py
+++ server-python/transcoder.py
@@ -1,47 +1,92 @@
 #!/usr/bin/env python
 
-import subprocess
+import os
+import shutil
+from subprocess import call
 
 from video_repr import Constants as Repr
+from video_repr import DefaultRepresentations
+
+
+def prepare_target_dir(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return
+
+    # make sure the directory exists
+    dir_name = os.path.dirname(os.path.realpath(file_path))
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
 
 
 def encode_x264(file_src, file_target, bitrate, fps, width, height, audio_asfq, audio_bitrate):
-    subprocess.Popen([
-        './tools/ix/convert.sh',
+    prepare_target_dir(file_target)
+
+    call('./tools/ix/convert.sh "%s" %d %s %dx%d %d %d "%s"' % (
         file_src,
-        str(bitrate),
+        bitrate,
         str(fps),
-        "%dx%d" % (width, height),
-        str(audio_asfq),
-        str(audio_bitrate),
+        width, height,
+        audio_asfq,
+        audio_bitrate,
         file_target
-    ], shell=True)
+    ), shell=True)
+
+    # since convert.sh does not return a meaningful exit code,
+    # we check the result file
+    return os.path.exists(file_target) and os.path.getsize(file_target) > 0
 
 
 def encode_x264_repr(file_src, file_target, video_repr):
-    encode_x264(file_src,
-                file_target,
-                video_repr.bandwidth / Repr.VIDEO_BIT_RATE_DIV,
-                Repr.VIDEO_FPS,
-                video_repr.width,
-                video_repr.height,
-                Repr.AUDIO_SAMPLE_FREQUENCY,
-                Repr.AUDIO_BITRATE)
+    return encode_x264(file_src,
+                       file_target,
+                       video_repr.bandwidth / Repr.VIDEO_BIT_RATE_DIV,
+                       Repr.VIDEO_FPS,
+                       video_repr.width,
+                       video_repr.height,
+                       Repr.AUDIO_SAMPLE_FREQUENCY,
+                       Repr.AUDIO_BITRATE)
 
 
 def encode_mp42ts(file_src, file_target):
-    subprocess.Popen(['./tools/ix/mp42ts', file_src, file_target], shell=True)
+    prepare_target_dir(file_target)
+    exit_code = call('mp42ts "%s" "%s"' % (file_src, file_target), shell=True)
+
+    # success exit code
+    return exit_code == 0
 
 
 if __name__ == "__main__":
     # test run
 
-    from video_repr import DefaultRepresentations
+    if os.path.exists('test_videos/output'):
+        shutil.rmtree('test_videos/output')
 
-    encode_x264_repr('test_videos/test_video.mp4', 'test_videos/LOW/test_video.mp4', DefaultRepresentations.LOW)
-    # encode_x264_repr('test_videos/test_video.mp4', 'test_videos/MEDIUM/test_video.mp4', DefaultRepresentations.MEDIUM)
-    # encode_x264_repr('test_videos/test_video.mp4', 'test_videos/HIGH/test_video.mp4', DefaultRepresentations.HIGH)
-    #
-    # encode_mp42ts('test_videos/LOW/test_video.mp4', 'test_videos/LOW/test_video.ts')
-    # encode_mp42ts('test_videos/MEDIUM/test_video.mp4', 'test_videos/MEDIUM/test_video.ts')
-    # encode_mp42ts('test_videos/HIGH/test_video.mp4', 'test_videos/HIGH/test_video.ts')
+    os.makedirs('test_videos/output/ORIGINAL')
+    os.makedirs('test_videos/output/HIGH')
+    os.makedirs('test_videos/output/MEDIUM')
+    os.makedirs('test_videos/output/LOW')
+
+    encode_x264_repr('test_videos/test_video.mp4',
+                     'test_videos/output/LOW/test_video.mp4',
+                     DefaultRepresentations.LOW)
+
+    encode_x264_repr('test_videos/test_video.mp4',
+                     'test_videos/output/MEDIUM/test_video.mp4',
+                     DefaultRepresentations.MEDIUM)
+
+    encode_x264_repr('test_videos/test_video.mp4',
+                     'test_videos/output/HIGH/test_video.mp4',
+                     DefaultRepresentations.HIGH)
+
+    encode_mp42ts('test_videos/test_video.mp4',
+                  'test_videos/output/ORIGINAL/test_video.ts')
+
+    encode_mp42ts('test_videos/output/LOW/test_video.mp4',
+                  'test_videos/output/LOW/test_video.ts')
+
+    encode_mp42ts('test_videos/output/MEDIUM/test_video.mp4',
+                  'test_videos/output/MEDIUM/test_video.ts')
+
+    encode_mp42ts('test_videos/output/HIGH/test_video.mp4',
+                  'test_videos/output/HIGH/test_video.ts')
